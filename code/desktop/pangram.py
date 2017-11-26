b55782f54db462aedfd6ba9fecc1bef2b6de4f4c
Cefn Hoile <github.com@cefn.com>
2017-11-26 14:34:11 +0000

    Useful pangram samples for test-running the display

---
+++ code/desktop/pangram.py
@@ -0,0 +1,11 @@
+examples = [
+    "Nymphs Blitz Quick Vex Dwarf jog",
+    "The Quick Brown Fox Jumped Over the Lazy Dog",
+    "Big Fjords Vex Quick Waltz Nymph",
+    "Brick Quiz Whangs Jumpy Veldt Fox",
+    "Glib Jocks Quiz Nymph To Vex Dwarf",
+    "Quick Wafting Zephyrs Vex Bold Jim",
+    "Fat Hag Dwarves Quickly Zap Jinx Mob",
+    "How Quickly Daft Jumping Zebras Vex",
+    "Five Hexing Wizard Bots Jump Quickly",
+]
