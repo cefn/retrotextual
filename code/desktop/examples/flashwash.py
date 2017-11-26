b9e7f84a0e6abb09fc046d704d90241b724f6d1c
Cefn Hoile <github.com@cefn.com>
2017-11-26 14:25:05 +0000

    Simplified to use Retrotextual utility methods

--- code/desktop/examples/flashwash.py
+++ code/desktop/examples/flashwash.py
@@ -1,35 +1,24 @@
-from emulator.graphicsDisplay import *
-from asyncio import sleep
+from display.gui import *
+from timing import sleep, complete
 
 display = GraphicsDisplay()
 
-def generateColorSequence():
-    while True:
-        for color in ["red", "green", "blue"]:
-            yield color
-
-colorSequence = generateColorSequence()
-
 async def scheduleWashes():
     while True:
-        color = next(colorSequence)
-        for character in display.characters:
-            for index in range(16):
-                character.setSegment(index, color, show=False)
-        display.show()
-        await asyncio.sleep(0.5)
+        for color in ["red", "green", "blue"]:
+            for character in display.characters:
+                for index in range(16):
+                    character.setSegment(index, color, show=False)
+            display.show()
+            await sleep(0.5)
 
 async def scheduleFlashes():
     while True:
-        color = next(colorSequence)
-        for character in display.characters[1::2]:
-            for index in range(16):
-                character.setSegment(index, color, show=False)
-        display.show()
-        await asyncio.sleep(0.1)
+        for color in ["red", "green", "blue"]:
+            for character in display.characters[1::2]:
+                for index in range(16):
+                    character.setSegment(index, color, show=False)
+            display.show()
+            await sleep(0.1)
 
-washSchedule = scheduleWashes()
-flashSchedule = scheduleFlashes()
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(washSchedule, flashSchedule))
+complete(scheduleWashes(), scheduleFlashes())
