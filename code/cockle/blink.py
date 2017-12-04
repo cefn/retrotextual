a840479db1ee8ae98197ff2151f7a34e649f1ac5
Cefn Hoile <github.com@cefn.com>
2017-12-04 14:57:12 +0000

    Blink behaviour

---
+++ code/cockle/blink.py
@@ -0,0 +1,9 @@
+from machine import Pin
+from time import sleep
+
+p = Pin(2, mode=Pin.OUT)
+while True:
+    p.value(0) # turn on (LED is reverse biased)
+    sleep(1)
+    p.value(1) # turn off
+    sleep(1)
