d1f40b2993e59f9975675868c5f63ce71dbf5135
Cefn Hoile <github.com@cefn.com>
2017-12-06 02:05:51 +0000

    Changed to service send behaviour in async routine, regulating number of segments sent

--- code/desktop/display/mqtt.py
+++ code/desktop/display/mqtt.py
@@ -1,19 +1,23 @@
 from display import *
 from display.gui import GraphicsDisplay
 from mqtt.hb import Link
-from timing import attempt
+from timing import attempt, sleep
 
 # TODO add clear behaviour which resets all segments, rather than lazy-resetting
 # TODO understand why Mosquitto's old published values remain after newly launched publishdisplay script should be reset on launch since all newColor values should differ from prevColor, forcing an MQTT message
 
 segmentTopicTemplate = "{}/{}"
+segmentPeriod = 0.05
+
 
 def marshallSegmentTopic(characterIndex, segmentIndex):
-    return segmentTopicTemplate.format(characterIndex,segmentIndex)
+    return segmentTopicTemplate.format(characterIndex, segmentIndex)
+
 
 def unmarshallSegmentTopic(topic):
     characterIndex, segmentIndex = [int(subpath) for subpath in topic.split('/')]
     return characterIndex, segmentIndex
+
 
 class MqttSenderCharacter(Character):
     def __init__(self, display, characterIndex):
@@ -28,37 +32,47 @@ class MqttSenderCharacter(Character):
         if show:
             self.show()
 
-    # TODO CH try to schedule an MQTT write in a round robin which limits the segment rate (e.g. adds a wait whenever a segment is found to have changed, and awaits the proper time for backoff)
     def show(self):
+        attempt(self.send())
+
+    async def send(self):
         '''Checks per segment if colors have changed, updates MQTT topics with changes to 3-byte color specs'''
-        for segmentIndex,nextColor in enumerate(self.nextColors):
-            prevColor= self.prevColors[segmentIndex]
+        for segmentIndex, nextColor in enumerate(self.nextColors):
+            prevColor = self.prevColors[segmentIndex]
             if nextColor != prevColor:
-                attempt(self.display.link.sendMessage(marshallSegmentTopic(self.characterIndex, segmentIndex), nextColor))
                 self.prevColors[segmentIndex] = nextColor
+                await self.display.link.sendMessage(marshallSegmentTopic(self.characterIndex, segmentIndex), nextColor)
+                await sleep(segmentPeriod)  # ensures an overall limit on segment dispatch rate
+
 
 class MqttSenderDisplay(Display):
-    def __init__(self, link, cols=10,rows=2):
+    def __init__(self, link, cols=10, rows=2):
         super().__init__(cols, rows)
         self.link = link
         for characterIndex in range(cols * rows):
             self.characters.append(MqttSenderCharacter(self, characterIndex))
 
     def show(self):
+        attempt(self.send())
+
+    async def send(self):
         for character in self.characters:
-            character.show()
+            await character.send()
+
 
 class MqttMonitor():
     def __init__(self, host=None):
-		if host is not None:
-			self.host = host
-		else:
-			self.host="localhost"
+        if host is not None:
+            self.host = host
+        else:
+            self.host = "localhost"
+
         self.guiDisplay = GraphicsDisplay()
         self.subscriptions = []
-        for characterIndex,guiCharacter in enumerate(self.guiDisplay.characters):
+        for characterIndex, guiCharacter in enumerate(self.guiDisplay.characters):
             for segmentIndex in range(16):
                 self.subscriptions.append(marshallSegmentTopic(characterIndex, segmentIndex))
+
 
     async def handleMessages(self):
         link = Link(self.host, self.subscriptions)
