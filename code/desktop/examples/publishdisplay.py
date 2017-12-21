15b29b88cf5c0c9870ec2a028a6351a41091ffb4
Cefn Hoile <github.com@cefn.com>
2017-12-21 16:54:05 +0000

    Added flashwash alternative (commented)

--- code/desktop/examples/publishdisplay.py
+++ code/desktop/examples/publishdisplay.py
@@ -1,6 +1,7 @@
 from mqtt.hb import Link
 from display.mqtt import MqttSenderDisplay
 from examples.hellofade import run
+#from examples.flashwash import run
 
 link = Link()
 display = MqttSenderDisplay(link)
