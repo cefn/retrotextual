da2bf2fd0667719fd6d1174232a97fe8fc745922
@ShrimpingIt Studio Laptop account <shrimpingit_studio_laptop@cefn.com>
2017-12-26 12:18:22 +0000

    Modified to support host specification

--- code/desktop/examples/subscribedisplay.py
+++ code/desktop/examples/subscribedisplay.py
@@ -2,13 +2,18 @@ from display.mqtt import MqttMonitor
 from timing import complete, sleep
 
 
-monitor = MqttMonitor()
+def run(host=None):
+	monitor = MqttMonitor(host)
 
-async def periodicDraw():
-    while True:
-        await sleep(0.1)
-        monitor.guiDisplay.show()
+	async def periodicDraw():
+		while True:
+			await sleep(0.1)
+			monitor.guiDisplay.show()
 
-messageCoro = monitor.handleMessages()
-drawCoro = periodicDraw()
-complete([messageCoro, drawCoro])
+	messageCoro = monitor.handleMessages()
+	drawCoro = periodicDraw()
+	complete([messageCoro, drawCoro])
+
+if __name__=="__main__":
+	run('localhost')
+#	run('10.42.0.1')
