2d4ff7423c8d5e79297c7e74d9a744410c6f1f12
Cefn Hoile <github.com@cefn.com>
2018-08-06 19:06:02 +0100

    Comment placeholder

---
+++ code/desktop/display/multi.py
@@ -0,0 +1,4 @@
+# Could create Multi-Display which relays to multiple sub-displays
+# This would allow a MqttSender to co-exist with a local GraphicDisplay
+# as well as an MqttMonitor which maintains a GraphicDisplay from
+# the topic/message information sent by the mosquitto broker
