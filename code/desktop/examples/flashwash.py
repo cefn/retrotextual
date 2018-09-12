f7537576880367e7ca6e23b497bbff33a304f5cf
Cefn Hoile <github.com@cefn.com>
2018-09-12 11:18:56 +0000

    Arbitrary changes to test sequence

--- code/desktop/examples/flashwash.py
+++ code/desktop/examples/flashwash.py
@@ -2,23 +2,27 @@ from display.gui import *
 from color import *
 from timing import sleep, forever
 
+#colors = [red, green, blue, yellow, teal, purple, white]
+colors = [white]
+colors = [red, green, blue]
+
 async def scheduleWashes(display):
     while True:
-        for color in [red, green, blue, yellow, teal, purple, white]:
+        for color in colors:
             for character in display.characters:
                 for index in range(16):
                     character.drawSegment(index, color, show=False)
             display.show()
-            await sleep(5.0)
+            await sleep(1.0)
 
 async def scheduleFlashes(display):
     while True:
-        for color in [red, green, blue, yellow, teal, purple, white]:
+        for color in colors:
             for character in display.characters[1::2]:
                 for index in range(16):
                     character.drawSegment(index, color, show=False)
             display.show()
-            await sleep(0.1)
+            await sleep(1.0)
 
 def run(display):
     async def render():
