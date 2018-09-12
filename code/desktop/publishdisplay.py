fc06d37b31f4c128f44f8bb4b4238a41160d3253
Cefn Hoile <github.com@cefn.com>
2018-09-12 13:15:28 +0000

    No longer need new location for publishdisplay.py subscribedisplay.py

--- code/desktop/publishdisplay.py
+++
@@ -1,9 +0,0 @@
-from mqtt.hb import Link
-from display.mqtt import MqttSenderDisplay
-#from examples.hellofade import run
-#from examples.flashwash import run
-from examples.test import run
-
-link = Link()
-display = MqttSenderDisplay(link)
-run(display)
